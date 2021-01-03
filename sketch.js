e01c3d093891a81c3260e601aec712dff416c417
Parikshit31 <72691401+Parikshit31@users.noreply.github.com>
2021-01-03 15:58:07 +0530

    tried using API calls

--- sketch.js
+++ sketch.js
@@ -7,11 +7,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score=0
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +46,27 @@ function setup(){
 }
 
 function draw(){
+    if (backgroundImg)
     background(backgroundImg);
+
+    noStroke();
+    textSize(30);
+    text("Score : " + score,width-300,30);
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
+    
     log3.display();
 
     box5.display();
@@ -84,5 +94,24 @@ function mouseReleased(){
 function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+    //console.log(responseJSON);
+
+    var daytime =  responseJSON.datetime
+    //console.log(daytime); 
+
+    var hour = daytime.slice(11,13);
+    console.log(hour);
+
+    if (hour>=6 && hour<=19){
+        backgroundImg = loadImage("sprites/bg.png");
+    }
+    else{
+        backgroundImg = loadImage("sprites/bg2.jpg");
     }
 }
